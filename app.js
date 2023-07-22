f65dbff28d51d72d0d6fcea03be89061979cfed5
cailaar <caila.alyssa.ryner@gmail.com>
2023-07-22 15:59:47 +0100

    form sections added

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-const form = document.getElementById("form");
+let currentSectionIndex = 0;
+const sections = document.getElementsByClassName("carousel-section");
+
+function showNextSection() {
+  if (currentSectionIndex < sections.length - 1) {
+    currentSectionIndex++;
+    transitionToSection();
+  }
+}
+
+function showPrevSection() {
+  if (currentSectionIndex > 0) {
+    currentSectionIndex--;
+    transitionToSection();
+  }
+}
+
+function transitionToSection() {
+  for (let i = 0; i < sections.length; i++) {
+    sections[i].style.display = i === currentSectionIndex ? "block" : "none";
+  }
+}
+
+function handleStart() {
+  document.getElementById("container").style.display = "none";
+  currentSectionIndex = 0;
+  transitionToSection();
+}
+
+function clearContainer() {
+  document.getElementById("container").style.display = "block";
+  currentSectionIndex = 0;
+  transitionToSection();
+}
